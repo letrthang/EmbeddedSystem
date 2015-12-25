acbc3569bafcb463375592b8298fd85b1ee3054c
Thang Le <letrthang@gmail.com>
2015-12-25 12:53:50 +0800

    1st commit

---
+++ MyARMProject/src/MyARMProject.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : MyARMProject.cpp
+ Author      : Thang Le
+ Version     :
+ Copyright   : MyARMProject
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "MyARMProject with C++" << endl; /* prints MyARMProject with C++ */
+	return 0;
+}
---
+++ MyARMProject/src/MyFunction.c
@@ -0,0 +1,15 @@
+/*
+ * MyFunction.c
+ *
+ *  Created on: Dec 22, 2015
+ *      Author: thangle
+ */
+
+
+#include <stdio.h>
+
+int MyFunction()
+{
+  printf("MyFunction in C\n");
+  return 0;
+}
